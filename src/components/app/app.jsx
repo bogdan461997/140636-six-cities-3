fd6ebc8d63dcfb3eb1eff543248a0a0f7b9b2704
bkostrov <bogdan.kostrov@gmail.com>
2020-02-11 17:38:02 +0300

    Добавил валидацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Main from "../main/main.jsx";
+import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prop-types
-const App = ({placesCount}) => {
-  return <Main placesCount={placesCount} />;
+const App = ({placesCount, placesInCity}) => {
+  return <Main placesCount={placesCount} placesInCity={placesInCity}/>;
+};
+
+App.propTypes = {
+  placesCount: PropTypes.number.isRequired,
+  placesInCity: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
 const PLACES_COUNT = 312;
-
+const PLACES_IN_CITY = [
+  `Beautiful & luxurious apartment at great location`,
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`,
+  `Wood and stone place`,
+];
 ReactDOM.render(
-    <App placesCount={PLACES_COUNT} />,
+    <App placesCount={PLACES_COUNT} placesInCity={PLACES_IN_CITY} />,
     document.getElementById(`root`)
 );
