8fefc887c3a0cf4467b8c9e21e259a0cddc3bb1f
bkostrov <bogdan.kostrov@gmail.com>
2020-02-14 12:24:34 +0300

    fix style

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import Main from "../main/main.jsx";
 import PropTypes from 'prop-types';
 
-const App = ({placesCount, placesInCity}) => {
-  return <Main placesCount={placesCount} placesInCity={placesInCity}/>;
+const App = ({placesInCity}) => {
+  return <Main placesInCity={placesInCity}/>;
 };
 
 App.propTypes = {
-  placesCount: PropTypes.number.isRequired,
   placesInCity: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 
-const Main = ({placesCount, placesInCity}) => {
+const Main = ({placesInCity}) => {
   return (
     <div className="page page--gray page--main">
       <header className="header">
@@ -69,7 +69,7 @@ const Main = ({placesCount, placesInCity}) => {
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{placesCount} places to stay in Amsterdam</b>
+              <b className="places__found">{placesInCity.length} places to stay in Amsterdam</b>
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
                 <span className="places__sorting-type" tabIndex="0">
@@ -93,7 +93,7 @@ const Main = ({placesCount, placesInCity}) => {
               </form>
               <div className="cities__places-list places__list tabs__content">
                 {placesInCity.map((placeName, index) => (
-                  <article key={index} className="cities__place-card place-card">
+                  <article key={placeName + index} className="cities__place-card place-card">
                     <div className="place-card__mark">
                       <span>Premium</span>
                     </div>
@@ -141,7 +141,6 @@ const Main = ({placesCount, placesInCity}) => {
 };
 
 Main.propTypes = {
-  placesCount: PropTypes.number.isRequired,
   placesInCity: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
