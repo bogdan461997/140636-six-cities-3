79a443afeab36545a3473c688c87f14f915bebb2
bkostrov <bogdan.kostrov@gmail.com>
2020-02-16 15:47:48 +0300

    Исправил замечания

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -4,6 +4,7 @@ import renderer from 'react-test-renderer';
 
 it(`App renders correctly`, () => {
   const placesInCity = [`Nice, cozy, warm big bed apartment`, `Wood and stone place`];
+  
   const tree = renderer
     .create(<App placesInCity={placesInCity}></App>)
     .toJSON();
